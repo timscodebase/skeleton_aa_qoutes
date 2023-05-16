8ccf6eba1ae0763f11b678e02a09205a990a010e
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-05-15 23:49:59 -0700

    base_url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const site_name = 'AA Quotes';
 export const description =
 	'A collection of quotes from AA and the Big Book of Alcoholics Anonymous';
-export const base_url = 'recoveryquotes-dsnfcq0vx-partners-in-code.vercel.app';
+export const base_url = 'https://recoveryquotes-idje4xql4-partners-in-code.vercel.app';
